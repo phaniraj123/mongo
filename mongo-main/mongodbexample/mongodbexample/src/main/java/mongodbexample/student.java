eefb21d16c45a4434f194d7e314c84b16f1d4334
phaniraj123 <140390293+phaniraj123@users.noreply.github.com>
2023-07-31 12:05:53 +0530

    Add files via upload

---
+++ mongo-main/mongodbexample/mongodbexample/src/main/java/mongodbexample/student.java
@@ -0,0 +1,145 @@
+package mongodbexample;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "students")
+public class student {
+    @Id
+    private int id;
+
+    private String name;
+  
+    private String city;
+
+    private String college;
+
+    private String Branch;
+
+    private float GPA;
+
+ 
+
+    public student(int id, String name, String city, String college, String Branch, float GPA) {
+
+        this.id = id;
+
+        this.name = name;
+
+        this.city = city;
+
+        this.college = college;
+
+        this.Branch = Branch;
+
+        this.GPA = GPA;
+
+    }
+
+ 
+
+    public int getId() {
+
+        return id;
+
+    }
+
+ 
+
+    public void setId(int id) {
+
+        this.id = id;
+
+    }
+
+ 
+
+    public String getName() {
+
+        return name;
+
+    }
+
+ 
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+ 
+
+    public String getCity() {
+
+        return city;
+
+    }
+
+ 
+
+    public void setCity(String city) {
+
+        this.city = city;
+
+    }
+
+ 
+
+    public String getCollege() {
+
+        return college;
+
+    }
+
+ 
+
+    public void setCollege(String college) {
+
+        this.college = college;
+
+    }
+
+ 
+
+    public String getStudentBranch() {
+
+        return Branch;
+
+    }
+
+ 
+
+    public void setStudentBranch(String Branch) {
+
+        this.Branch = Branch;
+
+    }
+
+ 
+
+    public float getStudentGPA() {
+
+        return GPA;
+
+    }
+
+ 
+
+    public void setStudentGPA(float GPA) {
+
+        this.GPA = GPA;
+
+    }
+
+ 
+
+    public Object getAllStudents() {
+
+        return null;
+
+    }
+
+ 
+
+}
